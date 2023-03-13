874bbabe769cd46fa0217cefe6cd5fb4e020e114
warutniy <125255821+warutniy@users.noreply.github.com>
2023-03-13 16:27:24 +0700

    Add files via upload

---
+++ function_inform.js
@@ -0,0 +1,4 @@
+
+
+const createInform = (firstName, location, hobby) => console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy "+hobby);
+createInform('Warut', 'Thailand', 'watching football matches.');
---
+++ function_manipulate.js
@@ -0,0 +1,15 @@
+
+let a = 10;
+let b = 2;
+
+const add = (a, b) => a + b;
+console.log(add(a, b));
+
+const subtract = (a, b) => a - b;
+console.log(subtract(a, b));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(a, b));
+
+const divide = (a, b) => a / b;
+console.log(divide(a, b));
